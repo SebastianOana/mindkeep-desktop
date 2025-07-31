83f79ab266af26a2214121ae9374a63e7e511209
Seba <oanasebastianflorin13@gmail.com>
2025-07-31 05:01:34 +0300

    Fixed buttons and added update feature

--- main.js
+++ main.js
@@ -1,49 +1,25 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
-let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
+    const win = new BrowserWindow({
         width: 1200,
         height: 800,
         webPreferences: {
             nodeIntegration: true,
-            contextIsolation: false,
-            webSecurity: false,
-            allowRunningInsecureContent: true
-        },
-        show: false
-    });
-
-    mainWindow.loadFile('index.html');
-    
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-        
-        // Only open DevTools in development
-        if (process.env.NODE_ENV === 'development') {
-            mainWindow.webContents.openDevTools();
+            contextIsolation: false
         }
     });
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+    win.loadFile('index.html');
+    win.webContents.openDevTools(); // Always show console
 }
 
-app.whenReady().then(() => {
-    createWindow();
-});
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
